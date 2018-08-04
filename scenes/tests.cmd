478931de77731f3bd10a97b1d2a65f5a4d9f5cac
TomCoded <tomtinkerer@gmail.com>
2018-08-04 19:20:35 +0000

    add tests

--- scenes/tests.cmd
+++ scenes/tests.cmd
@@ -1,3 +1,8 @@
 beautify -s spheres2.dat -n 25000 -N 30 -D 0.5
 beautify -s spheres2.dat -n 50000 -N 30
 beautify -s spheres3.dat -n 5000 -N 30
+beautify -s spheres4.dat -n 8000 -N 5
+beautify -s spheres5.dat -n 8000 -N 5
+beautify -s spheres6.dat -n 16000
+beautify -s scene1.dat -n 8000
+beautify -s scene3.dat -n 8000
