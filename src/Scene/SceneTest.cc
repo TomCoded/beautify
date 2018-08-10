2f3f04053162eabf8b43bac9f74dde02f36abc97
Tom White <tomtinkerer@gmail.com>
2018-08-09 17:24:41 -0700

    test: set up GLUT initialization for SceneTest tests.
    Also test Scene->Readfile.

--- src/Scene/SceneTest.cc
+++ src/Scene/SceneTest.cc
@@ -1,10 +1,43 @@
 #include "Scene.h"
+#include <GL/glut.h>
+#include <gtest/gtest.h>
+#include <string>
+#include <unistd.h>
+
+void frameCount();
+void display();
+void keyboard();
+void reshape();
+extern bool g_suppressGraphics;
+extern bool g_parallel;
+
+bool glutInitialized=false;
 
 namespace {
-  class SceneTest : public ::testing::test {
+  class SceneTest : public ::testing::Test {
   protected:
     SceneTest() {
+      g_suppressGraphics=false;
+      g_parallel=false;
+
+      if(!glutInitialized) {
+	char *myargv [1];
+	int myargc=1;
+	myargv [0]=strdup ("beautify");
+	glutInit(&myargc, myargv);
+	glutInitialized=true;
+      }
+      
+      scene = new Scene();
     }
+
+    ~SceneTest() {
+      //delete scene;
+    }
+
+    const char * workingDir="scenes";
+    std::string filename="largescene1.dat";
+    Scene * scene;
     
   };
 }
@@ -13,6 +46,8 @@ TEST_F(SceneTest,defaultConstructor) {
 }
 
 TEST_F(SceneTest,ReadFile) {
+  EXPECT_EQ(chdir(workingDir),0);
+  scene->ReadFile(filename);
 }
 
 TEST_F(SceneTest,WriteImage) {
